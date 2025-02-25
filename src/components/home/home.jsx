60fc79dc5d970764636aedb76516c81d79c13fb4
Qodirxon2000a <ibshdh711@gmail.com>
2025-02-25 22:51:32 +0500

    A

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,45 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./home.css";
 
 const FootballStream = () => {
-  const [login, setLogin] = useState("");
-  const [password, setPassword] = useState("");
   const navigate = useNavigate();
-
-  const handleLogin = () => {
-    if (login === "1" && password === "1") {
-      navigate("/dash");
-    } else if (login === "2" && password === "2") {
-      navigate("/view");
-    } else if (login === "3" && password === "3") {
-      navigate("/calcu");
-    } else if (login === "4" && password === "4") { // Bu yer to'g'rilandi
-      navigate("/money");
-    } else {
-      alert("Login yoki parol noto‘g‘ri!");
-    }
-  };
-  
 
   return (
     <div className="login-container">
       <div className="login-box">
-        <div className="logo"></div> {/* Logo joyi */}
-        <h2>Kirish</h2>
-        <input 
-          type="text" 
-          placeholder="Login" 
-          value={login} 
-          onChange={(e) => setLogin(e.target.value)}
-        />
-        <input 
-          type="password" 
-          placeholder="Parol" 
-          value={password} 
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button onClick={handleLogin}>Kirish</button>
+        <h2>Bo'limlar</h2>
+        <button onClick={() => navigate("/dash")} className="nav-btn">Boshqaruv</button>
+        <button onClick={() => navigate("/view")} className="nav-btn">Ishga Yuborish</button>
+        <button onClick={() => navigate("/calcu")} className="nav-btn">Hisoblash</button>
+        <button onClick={() => navigate("/money")} className="nav-btn">Oylik</button>
       </div>
     </div>
   );
